4a909873144da91a1113f16a27213dcf26ec07dd
iWinniePooh <python@rupsy.de>
2023-07-11 21:00:09 +0200

    temp commit

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,31 @@
-import type { NextPage } from "next";
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { Metadata } from 'next';
 
-const ProductsPage: NextPage = () => {
-  return (
-    <>
-    <h1>Hello Products page</h1>
-    </>
-  )
+export const metadata: Metadata = {
+  title: 'Products catalog',
+};
+
+interface filtersType {
+  [key: string]: string;
 }
 
-export default ProductsPage
+export default function ProductsPage() {
+  const filters: filtersType = {};
+  for (const [key, value] of useSearchParams().entries()) {
+    filters[key] = value;
+  }
+
+  return (
+    <>
+      <h1>Hello Products page</h1>
+      <ul>
+        {Object.entries(filters)?.map((item) => (
+          <li key={item[0]}>
+            {item[0]}: {item[1]}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
